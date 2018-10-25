32dca02117602c07e508dcb7665e9e35b9005616
johnyx98 <43720611+johnyx98@users.noreply.github.com>
2018-10-24 20:48:23 -0700

    All Projects from CSE20

---
+++ Lab1_Basic_Output/First_Program.java
@@ -0,0 +1,15 @@
+
+public class First_Program {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print ("Hello, ");
+		System.out.println ("World");
+		System.out.print ("My name is Johny!");
+		System.out.println ("Welcome to CSE20 at UC Merced");
+	}
+
+}
---
+++ Lab2_Getting_to_know_everything/Interview.java
@@ -0,0 +1,18 @@
+
+public class Interview {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(" Interview with Danny Her.");
+		System.out.println(" Why did you choose your major? ");
+		System.out.print(" It's fun.");
+		System.out.println(" Do you work well with other people? ");
+		System.out.print(" Yes I do.");
+	
+
+	}
+
+}
